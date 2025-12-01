3362e6538318becbd1265af76812ed9d9f6f1c4c
Christopher90434 <christopherbryan.susanto@gmail.com>
2025-12-01 20:10:49 +0700

    "Commit No. 3 – Assignment"

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -3,21 +3,24 @@ package com.example.test_lab_week_12
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class MovieRepository(private val movieService: MovieService) {
     private val apiKey = "16d5967115a79415befa8533d37791b0"
 
-    // LiveData untuk menyimpan list movies
+    // LiveData untuk Part 1
     private val movieLiveData = MutableLiveData<List<Movie>>()
     val movies: LiveData<List<Movie>>
         get() = movieLiveData
 
-    // LiveData untuk error message
     private val errorLiveData = MutableLiveData<String>()
     val error: LiveData<String>
         get() = errorLiveData
 
-    // Fetch movies dari API menggunakan suspend function
+    // Fetch movies dengan LiveData (Part 1)
     suspend fun fetchMovies() {
         try {
             val popularMovies = movieService.getPopularMovies(apiKey)
@@ -25,5 +28,12 @@ class MovieRepository(private val movieService: MovieService) {
         } catch (exception: Exception) {
             errorLiveData.postValue("An error occurred: ${exception.message}")
         }
+    }
+
+    // Fetch movies dengan Flow (Part 2)
+    fun fetchMoviesFlow(): Flow<List<Movie>> {
+        return flow {
+            emit(movieService.getPopularMovies(apiKey).results)
+        }.flowOn(Dispatchers.IO)
     }
 }
